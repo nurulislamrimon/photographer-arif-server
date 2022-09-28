4572d995b9b222c6a4fb8cf584711ca57e299d7d
Nurul Islam Rimon <nurulislamrimon@gmail.com>
2022-09-28 13:19:58 +0600

    photos crud operation done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config()
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors())
@@ -15,13 +15,58 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
+        const carouselCollection = client.db('carouselCollection').collection('carousel');
         const photoCollection = client.db('photoCollection').collection('photo');
+        const achivementCollection = client.db('achivementCollection').collection('achivement');
 
+        app.get('/carousel', async (req, res) => {
+            const query = {};
+            const result = await carouselCollection.find(query).toArray();
+            res.send(result);
+            console.log('carousels responsed');
+        })
+        // photos crud
         app.get('/photos', async (req, res) => {
             const query = {};
-            const result = await photoCollection.find(query).toArray();
+            const result = await photoCollection.find(query).sort({ _id: 1 }).toArray();
             res.send(result);
             console.log('Photos responsed');
+        })
+        // specific photo
+        app.get('/photo', async (req, res) => {
+            const id = req.query.id;
+            const query = { _id: ObjectId(id) }
+            const result = await photoCollection.findOne(query);
+            res.send(result)
+            console.log(`photo ${id} is responsing`);
+        })
+        app.post('/photo', async (req, res) => {
+            const newPhoto = req.body.data;
+            const result = await photoCollection.insertOne(newPhoto);
+            res.send(result);
+            console.log('new photo added');
+        })
+        app.delete('/photo/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await photoCollection.deleteOne(query);
+            res.send(result);
+            console.log(`photo ${id} deleted`);
+        })
+        app.put('/photo/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateData = req.body;
+            const query = { _id: ObjectId(id) };
+            const result = await photoCollection.updateMany(query, { $set: updateData })
+            res.send(result);
+            console.log(`photo ${id} is updated`);
+        })
+        // achivements crud
+        app.get('/achivements', async (req, res) => {
+            const query = {};
+            const result = await achivementCollection.find(query).toArray();
+            res.send(result);
+            console.log('achivements responsed');
         })
     } finally { }
 }
